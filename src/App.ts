6efd943a3e925ac95312e0f099acedea75375e73
ldwqh0 <ldwqh0@outlook.com>
2018-04-05 12:46:42 +0800

    修改测试代码

--- src/App.ts
+++ src/App.ts
@@ -2,14 +2,15 @@
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 import { ScriptLoader } from 'vue-esri'
-// import VMap from 'vue-esri'
-// Vue.use
 @Component({
   template: `<div><v-map style='height:500px;'/></div>`
 })
 export default class App extends Vue {
   name = 'App'
   mounted() {     
-    // ScriptLoader.loadModules
+    ScriptLoader.loadModules(['esri/map']).then(([Map])=>{ // 这里能够正确识别定义和导出
+      console.dir(Map) 
+    })
+    console.log(this.$esri)  // 这里报告没有相关属性
   }
 }
